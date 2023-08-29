95e70aa0573d0521794998f64e24503ee014baf0
KishoreXLR8 <kishoresworks8@gmail.com>
2023-08-29 20:59:55 +0530

    Added expense button usable

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -22,20 +22,38 @@ const [enteredDate,setEnteredDate] = useState('')
     setEnteredDate(event.target.value);
   };
 
+ 
+
+ function submitHandler (event){
+  event.preventDefault();
+
+  const expenseData = {
+    title : enteredTitle,
+    amount: enteredAmount,
+    date: new Date(enteredDate)
+
+  };
+
+  console.log(expenseData);
+  setEnteredAmount('');
+  setEnteredDate('');
+  setEnteredTitle('');
+ };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text'  onChange={titleChangeHandler}/>
+          <input type='text' value={enteredTitle}  onChange={ titleChangeHandler}/>
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' onChange={amountCHangeHandler} />
+          <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountCHangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateCHangeHandler} />
+          <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateCHangeHandler} />
         </div>
       </div>
       <div className='new-expense__actions'>
