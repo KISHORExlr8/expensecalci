1a74de42d667f1158b59f71376eeb8f5877a4d58
KishoreXLR8 <kishoresworks8@gmail.com>
2023-08-30 21:29:46 +0530

    Constructed child-parent relationship

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 const [enteredTitle,setEnteredTitle] = useState('')
 const [enteredAmount,setEnteredAmount] = useState('')
 const [enteredDate,setEnteredDate] = useState('')
@@ -34,7 +34,7 @@ const [enteredDate,setEnteredDate] = useState('')
 
   };
 
-  console.log(expenseData);
+  props.onSaveExpenseData(expenseData);
   setEnteredAmount('');
   setEnteredDate('');
   setEnteredTitle('');
