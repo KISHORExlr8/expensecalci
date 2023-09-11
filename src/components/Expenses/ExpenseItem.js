0cc5d1386c15549506cb9b2302f0dfa2a0d0822d
KishoreXLR8 <kishoresworks8@gmail.com>
2023-09-11 22:30:17 +0530

    adding new expenses

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,24 +6,17 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
-  const [title,setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by react');
-
-  const clickHandler = () => {
-    setTitle('Updated...!');
-    console.log(title);
-
-  };
   return (
     <div className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
     </div>
   );
 }
+  
+  
 
 export default ExpenseItem;
